ed01bf266589f37266f8d6e073093cc272eb656c
15-dy1005la <15-dy1005la@LAPTOP-2BG3B94I>
2022-04-07 00:58:41 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace tienda_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ACCEDER = new System.Windows.Forms.Button();
+            this.SALIR = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTRASEÑA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(223, 168);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.TabIndex = 2;
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Location = new System.Drawing.Point(223, 236);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtcontraseña.TabIndex = 3;
+            this.txtcontraseña.UseSystemPasswordChar = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(207, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ACCEDER
+            // 
+            this.ACCEDER.Location = new System.Drawing.Point(159, 292);
+            this.ACCEDER.Name = "ACCEDER";
+            this.ACCEDER.Size = new System.Drawing.Size(99, 31);
+            this.ACCEDER.TabIndex = 5;
+            this.ACCEDER.Text = "ACCEDER";
+            this.ACCEDER.UseVisualStyleBackColor = true;
+            this.ACCEDER.Click += new System.EventHandler(this.ACCEDER_Click);
+            // 
+            // SALIR
+            // 
+            this.SALIR.Location = new System.Drawing.Point(283, 292);
+            this.SALIR.Name = "SALIR";
+            this.SALIR.Size = new System.Drawing.Size(99, 31);
+            this.SALIR.TabIndex = 6;
+            this.SALIR.Text = "SALIR";
+            this.SALIR.UseVisualStyleBackColor = true;
+            this.SALIR.Click += new System.EventHandler(this.SALIR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(547, 357);
+            this.Controls.Add(this.SALIR);
+            this.Controls.Add(this.ACCEDER);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SistemadeCompra";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ACCEDER;
+        private System.Windows.Forms.Button SALIR;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tienda_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ACCEDER_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = txtusuario.Text;
+            pass = txtcontraseña.Text; 
+            if (user == "Tienda" && pass == "1234") 
+            {
+                MessageBox.Show("Bienvenido");
+            }
+            else 
+            {
+                MessageBox.Show("Error en contraseña");
+                txtusuario.Text = "";
+                txtcontraseña.Text = "";
+            }
+        }
+
+        private void SALIR_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
